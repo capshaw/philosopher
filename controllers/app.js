44e926095f7c95d60af44d574516eff28b7c9a83
Andrew Capshaw <andrew.capshaw@gmail.com>
2016-07-24 12:31:31 -0500

    Modify healthcheck

--- controllers/app.js
+++ controllers/app.js
@@ -1,11 +1,14 @@
 const config = require('../config');
+const os = require('os');
 
 module.exports = (app, now) => {
   app.get('/', (req, res) => {
     res.send({
       name: 'philosopher',
-      purpose: 'tbd',
+      purpose: 'A service for @acapshaw to learn node',
+      documentation: 'https://github.com/capshaw/philosopher',
       startup: now,
+      host: os.hostname(),
       properties: {
         port: config.port,
         environment: config.environment,
